90b5793ecc24fb8fe3fef34d87f0981e3e84c7c9
uo270235 <uo270235@uniovi.es>
2024-06-24 11:12:12 +0200

    cambio a un solo botn de descarga

--- TFG_UO270235/src/components/editor.js
+++ TFG_UO270235/src/components/editor.js
@@ -2,13 +2,11 @@ import React, { useState, useEffect, useRef } from 'react';
 import './editor.css';
 import EditorYashe from './yashe';
 import shumlex from 'shumlex';
-// import PlantUMLParser from '../parserShapes';
 import Diagram from './Diagram';
 import Alerta from './Alerta';
 import { FaDownload } from 'react-icons/fa';
 import { callApi } from '../ApiManager';
 import { PlantUMLGenerator } from '../PlantUmlGenerator';
-
 
 function Editor() {
   const editorRef = useRef(null);
@@ -48,6 +46,7 @@ prefix xsd: <http://www.w3.org/2001/XMLSchema#>
       editorRef.current.setYasheValue(example);
     }, 1);
   }, []);
+
   const recreateMermaid = async() => {
     console.log("DEBERIA RECREARLO");
     const container = document.getElementById('mermaid-diagram-container');
@@ -59,18 +58,18 @@ prefix xsd: <http://www.w3.org/2001/XMLSchema#>
     newMermaidDiagram.id = 'mermaid-diagram';
     container.insertBefore(newMermaidDiagram, container.firstChild);
   };
-  const createKroki = async (json_api,mermaidUML) => {
+
+  const createKroki = async (json_api, mermaidUML) => {
     try {
       console.log("big ball of mud");
       console.log(json_api);
-      const generator = new PlantUMLGenerator(json_api,mermaidUML);
+      const generator = new PlantUMLGenerator(json_api, mermaidUML);
       const plantUMLCode = generator.generate(); 
       console.log("kroki source!!!!!!!!!!!!");
       console.log(plantUMLCode);
       setKrokiSource(plantUMLCode);
       setParseError(null);
-      if(json_api.length==0)
-        setKrokiSource();
+      if (json_api.length === 0) setKrokiSource('');
       return plantUMLCode;
     } catch (error) {
       console.error("Error al parsear ShEx:", error);
@@ -89,11 +88,6 @@ prefix xsd: <http://www.w3.org/2001/XMLSchema#>
     }
   };
 
-  /**
-   * 
-   * @param {*} svgContent 
-   * @param {*} filename 
-   */
   const downloadDiagram = (svgContent, filename) => {
     if (svgContent) {
       const blob = new Blob([svgContent], { type: 'image/svg+xml' });
@@ -107,15 +101,7 @@ prefix xsd: <http://www.w3.org/2001/XMLSchema#>
       document.body.removeChild(link);
       URL.revokeObjectURL(url);
     } else {
-      console.error('No Relational UML diagram content available.');
-    }
-  };
-
-  const downloadKrokiDiagram = () => {
-    if (krokiSvg) {
-      downloadDiagram(krokiSvg, 'diagramLogicUMLClass');
-    } else {
-      console.error('No Logic UML diagram content available.');
+      console.error('No hay contenido del diagrama UML relacional disponible.');
     }
   };
 
@@ -123,7 +109,7 @@ prefix xsd: <http://www.w3.org/2001/XMLSchema#>
     try {
       let yasheValue = editorRef.current.getYasheValue();
       let source = yasheValue.replace(/\bnot\b/gi, "NOT");
-      let mermaidUML = shumlex.shexToUML("mermaid-diagram",source);
+      let mermaidUML = shumlex.shexToUML("mermaid-diagram", source);
       shumlex.asignarEventos('mermaid-diagram');
       setIsMermaidDiagramVisible(true);
       return mermaidUML;
@@ -136,25 +122,68 @@ prefix xsd: <http://www.w3.org/2001/XMLSchema#>
     }
   };
 
+  const getSvgDimensions = (svgString) => {
+    const parser = new DOMParser();
+    const svgDoc = parser.parseFromString(svgString, "image/svg+xml");
+    const svgElement = svgDoc.getElementsByTagName('svg')[0];
+    const width = parseFloat(svgElement.getAttribute('width') || svgElement.viewBox.baseVal.width);
+    const height = parseFloat(svgElement.getAttribute('height') || svgElement.viewBox.baseVal.height);
+    return { width, height };
+  };
+
+  const combineSVGs = () => {
+    const mermaidSVG = document.getElementById('mermaid-diagram')?.innerHTML || '';
+    let krokiSVG = krokiSvg || '';
+
+    // Elimina las declaraciones XML adicionales
+    krokiSVG = krokiSVG.replace(/<\?xml.*?\?>/g, '');
+
+    if (!mermaidSVG && !krokiSVG) {
+      console.error('No hay contenido SVG para combinar.');
+      return;
+    }
+
+    const krokiDimensions = krokiSVG ? getSvgDimensions(krokiSVG) : { width: 0, height: 0 };
+    const mermaidDimensions = mermaidSVG ? getSvgDimensions(mermaidSVG) : { width: 0, height: 0 };
+
+    const combinedWidth = Math.max(krokiDimensions.width, mermaidDimensions.width);
+    const combinedHeight = krokiDimensions.height + mermaidDimensions.height;
+
+    const combinedSVG = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="${combinedWidth}" height="${combinedHeight}">
+        ${krokiSVG ? `<g>${krokiSVG}</g>` : ''}
+        ${mermaidSVG ? `<g transform="translate(0, ${krokiDimensions.height})">${mermaidSVG}</g>` : ''}
+      </svg>
+    `;
+
+    downloadDiagram(combinedSVG, 'combinedDiagram');
+  };
+
   return (
     <>
-    <div className="container">
-      <div className='editor'>
-        <h1 className="page-title">Schema (ShEx)</h1>
-        <EditorYashe ref={editorRef} />
-        <div className='editor-buttons'>
-          <button className='button-20' onClick={async () => {
+      <div className="container">
+        <div className='editor'>
+          <h1 className="page-title">Schema (ShEx)</h1>
+          <EditorYashe ref={editorRef} />
+          <div className='editor-buttons'>
+            <button className='button-20' onClick={async () => {
               const yasheValue = editorRef.current.getYasheValue();
               try {
                 await recreateMermaid();
+                
+                // Limpiar estado previo de Kroki
+                setKrokiSource('');
+                setKrokiSvg('');
+                setIsKrokiDiagramVisible(false);
+                
                 //Obtenemos JSON a través de la API
                 let json_api = await callApi(yasheValue); 
 
                 // Generación de Mermaid
-                let mermaidUML=createMermaid();
+                let mermaidUML = createMermaid();
 
                 // Generación de Kroki
-                createKroki(json_api,mermaidUML);
+                createKroki(json_api, mermaidUML);
 
               } catch (error) {
                 console.error("Error al generar Plant UML:", error);
@@ -164,29 +193,26 @@ prefix xsd: <http://www.w3.org/2001/XMLSchema#>
                 clearMermaidDiagram();
               }
             }}>
-            Ver Diagrama
-          </button>
+              Ver Diagrama
+            </button>
+          </div>
         </div>
-           </div>
-           {parseError && (
-        <Alerta mensaje={`Error al parsear ShEx,revise su entrada.`} onClose={() => setParseError(null)} />
-      )}
-              <div className="result-container">
+        {parseError && (
+          <Alerta mensaje={`Error al parsear ShEx, revise su entrada.`} onClose={() => setParseError(null)} />
+        )}
+        <div className="result-container">
           {krokiSource && 
           <div className="diagram-container kroki-diagram" data-zoom-on-wheel="zoom-amount: 0.01; min-scale: 0.3; max-scale: 20;" data-pan-on-drag>
             <Diagram diagramSource={krokiSource} onSvgGenerated={setKrokiSvg} />
-            <div className='icon-container'>
-              <button className='download-icon' onClick={downloadKrokiDiagram}>
-                <FaDownload />
-              </button>
-            </div>
           </div>}
           <div id="mermaid-diagram-container" className={isMermaidDiagramVisible ? (krokiSource ? "diagram-container mermaid-diagram" : "diagram-container only-mermaid") : ""} data-zoom-on-wheel="zoom-amount: 0.01; min-scale: 0.3; max-scale: 20;" data-pan-on-drag>
-          <div id="mermaid-diagram"></div>
-              <button className='download-icon' onClick={() => downloadDiagram(document.getElementById('mermaid-diagram').innerHTML, 'diagramUMLRelationalClass')}>
-                <FaDownload />
-              </button>
+            <div id="mermaid-diagram"></div>
           </div>
+        </div>
+        <div className='icon-container'>
+          <button className='download-icon' onClick={combineSVGs}>
+            <FaDownload />
+          </button>
         </div>
       </div>
     </>
